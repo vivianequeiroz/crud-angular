19995e5012c9438f059aceef371f346231f14b33
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-30 12:46:51 -0300

    chore: adapt angular schematics table to product model as reference

--- frontend/src/app/components/product/products-table/products-table.component.ts
+++ frontend/src/app/components/product/products-table/products-table.component.ts
@@ -1,22 +1,23 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { ProductsTableDataSource, ProductsTableItem } from './products-table-datasource';
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTable } from "@angular/material/table";
+import { Product } from "../product.model";
+import { ProductsTableDataSource } from "./products-table-datasource";
 
 @Component({
-  selector: 'app-products-table',
-  templateUrl: './products-table.component.html',
-  styleUrls: ['./products-table.component.css']
+  selector: "app-products-table",
+  templateUrl: "./products-table.component.html",
+  styleUrls: ["./products-table.component.css"],
 })
 export class ProductsTableComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatTable) table: MatTable<ProductsTableItem>;
+  @ViewChild(MatTable) table: MatTable<Product>;
   dataSource: ProductsTableDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ["id", "name", "price"];
 
   ngOnInit() {
     this.dataSource = new ProductsTableDataSource();
