336a33c1acfa4ef92213ea50c185533e8b05ae28
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-30 11:00:24 -0300

    feat: add routing from products to create products page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatCardModule} from '@angular/material/card';
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent} from './components/template/nav/nav.component';
+import { MatCardModule } from "@angular/material/card";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatButtonModule } from "@angular/material/button";
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './views/home/home.component';
-import { ProductsComponent } from './views/products/products.component';
-import { RedColorDirective } from './directives/red-color.directive';
-import { CreateProductComponent } from './components/products/create-product/create-product.component';
+import { NavComponent } from "./components/template/nav/nav.component";
+import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+
+import { HomeComponent } from "./views/home/home.component";
+import { ProductsComponent } from "./views/products/products.component";
+import { CreateProductComponent } from "./components/products/create-product/create-product.component";
+
+import { RedColorDirective } from "./directives/red-color.directive";
 
 @NgModule({
   declarations: [
@@ -27,7 +30,7 @@ import { CreateProductComponent } from './components/products/create-product/cre
     HomeComponent,
     ProductsComponent,
     RedColorDirective,
-    CreateProductComponent
+    CreateProductComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,9 +39,10 @@ import { CreateProductComponent } from './components/products/create-product/cre
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/views/products/products.component.ts
+++ frontend/src/app/views/products/products.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  navigateToCreateProduct(): void {
+    this.router.navigate(["/products/create"]);
   }
-
 }
